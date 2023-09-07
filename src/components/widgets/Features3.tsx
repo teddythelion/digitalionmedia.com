fe7292810e025de9628f0ce4058b68dc01fa2363
Teddy A. DeLeon (The Lion) <teddydeleon1976@gmail.com>
2023-09-07 11:33:51 -0700

    Update Features3.tsx

--- src/components/widgets/Features3.tsx
+++ src/components/widgets/Features3.tsx
@@ -39,7 +39,7 @@ const Features3 = ({ header, items }: FeaturesProps) => (
                       type="button"
                       className=" inline-block rounded border-2 border-primary-100 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:border-primary-accent-100 hover:bg-neutral-500 hover:bg-opacity-10 focus:border-primary-accent-100 focus:outline-none focus:ring-0 active:border-primary-accent-200 dark:text-primary-100 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                         data-te-ripple-init>
-                               <a className="flex font-bold text-blue-600 hover:underline dark:text-gray-200 mb-2" href={link.href}> {link.label}</a>   
+                               <a className="flex font-bold text-blue-200 hover:underline dark:text-white mb-2" href={link.href}> {link.label}</a>   
                       </button>
                   </div>
                 )}
