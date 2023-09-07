bed406298508d1edd7ddc631d14ce5b1fbb35810
Teddy A. DeLeon (The Lion) <teddydeleon1976@gmail.com>
2023-09-07 15:02:47 -0700

    Update CallToAction2.tsx

--- src/components/widgets/CallToAction2.tsx
+++ src/components/widgets/CallToAction2.tsx
@@ -6,17 +6,17 @@ const Card = ({ title, description, href, form }: Item) => (
     <div className="flex items-center justify-between">
       <div className="w-full">
         <h3 className="mb-3 text-sm font-bold dark:text-white">{title}</h3>
-        <p className="text-gray-600 dark:text-slate-400">{description}</p>
+        <p className="text-gray-100 dark:text-slate-400">{description}</p>
       </div>
       {href && (
         <div className="flex h-10 w-10 items-center justify-center">
-          <IconChevronRight className="h-6 w-6 text-primary-600 dark:text-slate-200" />
+          <IconChevronRight className="h-6 w-6 text-white dark:text-slate-200" />
         </div>
       )}
     </div>
     {form && (
       <div className="mt-2">
-        <form className="rounded-md border border-gray-400 bg-white shadow-md">
+        <form className="rounded-md border border-gray-400 bg-slate-600 shadow-md">
           <div className="flex items-center">
             {form.icon && (
               <span className="rounded-tl rounded-bl border-r-[1px] border-gray-400 px-2 py-2 dark:bg-[#3b3b3b]">
