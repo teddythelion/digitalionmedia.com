cb85699a75cf69669a76053a51d3ee8283243957
Teddy A. DeLeon (The Lion) <teddydeleon1976@gmail.com>
2023-09-07 15:20:44 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,23 +21,9 @@ export default function RootLayout({ children }: LayoutProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
 <link rel="preconnect" href="https://fonts.gstatic.com" ></link>
 <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300&family=Montserrat+Subrayada&display=swap" rel="stylesheet"></link>
-     <!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-5XGVW72');
+    
 
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
 		
-		  gtag('config', 'G-N3ZHJ9GWKC');
-		
-		</script>
-		<!-- Google tag (gtag.js) -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-N3ZHJ9GWKC"></script>
-		<!-- End Google Tag Manager -->
     </head>
       <body className="bg-white tracking-tight text-gray-100 antialiased dark:bg-slate-900 dark:text-slate-100"   style={{
       backgroundColor:"black",
